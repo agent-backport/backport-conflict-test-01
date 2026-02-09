e97156df9e10d685437154949f87deec2b6f4a3c
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2026-02-08 22:11:47 -0800

    Add enhanced password validation for v2.0
    - Increase minimum password length to 12 characters
    - Add complexity requirements: uppercase, lowercase, number, special character

--- password-validator.js
+++ password-validator.js
@@ -1,5 +1,5 @@
 /**
- * Password Validator v1.0
+ * Password Validator v2.0
  *
  * Validates user passwords according to security requirements.
  */
@@ -7,9 +7,29 @@
 function validatePassword(password) {
   const errors = [];
 
-  // Check minimum length
-  if (password.length < 8) {
-    errors.push('Password must be at least 8 characters long');
+  // Check minimum length (v2.0: increased to 12 characters)
+  if (password.length < 12) {
+    errors.push('Password must be at least 12 characters long');
+  }
+
+  // Check for uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    errors.push('Password must contain at least one uppercase letter');
+  }
+
+  // Check for lowercase letter
+  if (!/[a-z]/.test(password)) {
+    errors.push('Password must contain at least one lowercase letter');
+  }
+
+  // Check for number
+  if (!/[0-9]/.test(password)) {
+    errors.push('Password must contain at least one number');
+  }
+
+  // Check for special character
+  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    errors.push('Password must contain at least one special character');
   }
 
   if (errors.length > 0) {
